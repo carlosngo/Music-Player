41d761c3091d54b545f1b5db9dcb0131aa26a343
samuraimac <39005419+samuraimac@users.noreply.github.com>
2019-02-25 13:54:45 +0800

    Add files via upload

---
+++ src/View/SettingsWindow.java
@@ -0,0 +1,21 @@
+package View;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Paths;
+import java.text.*;
+import java.util.*;
+import javax.swing.*;
+
+public class SettingsWindow extends JFrame {
+
+    public SettingsWindow(Point pt){
+        JPanel p = new JPanel();
+        setLocation(pt);
+        pack();
+    }
+
+}
