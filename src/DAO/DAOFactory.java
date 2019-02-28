23e138742a56ed05a2189158c94acda38e712fe0
Carlos <carlos_ngo@dlsu.edu.ph>
2019-02-28 20:41:22 +0800

    Update DAOFactory.java

--- src/DAO/DAOFactory.java
+++ src/DAO/DAOFactory.java
@@ -4,23 +4,23 @@ import java.sql.*;
 
 public abstract class DAOFactory {
 
-    public static final String USER_TABLE = "musicplayer.User";
+    public static final String USER_TABLE = "musicplayer.user";
     public static final String USER_COLUMNS =
             "PK_UserID, Username, Password, FirstName, LastName, Birthday";
 
-    public static final String SONG_TABLE = "musicplayer.Song";
+    public static final String SONG_TABLE = "musicplayer.song";
     public static final String SONG_COLUMNS =
             "PK_SongID, FK_UserID, FK_AlbumID, FK_GenreID, Name, Year, Favorite, PlayTime, LastPlayed, Path";
 
-    public static final String PLAYLIST_TABLE = "musicplayer.Playlist";
+    public static final String PLAYLIST_TABLE = "musicplayer.playlist";
     public static final String PLAYLIST_COLUMNS =
             "PK_PlaylistID, FK_UserID, Name, Favorite";
 
-    public static final String GENRE_TABLE = "musicplayer.Genre";
+    public static final String GENRE_TABLE = "musicplayer.genre";
     public static final String GENRE_COLUMNS =
             "PK_GenreID, FK_UserID, Name";
 
-    public static final String ALBUM_TABLE = "musicplayer.Album";
+    public static final String ALBUM_TABLE = "musicplayer.album";
     public static final String ALBUM_COLUMNS =
             "PK_AlbumID, FK_UserID, Name, Artist, Cover";
 
