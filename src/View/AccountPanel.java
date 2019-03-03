9538d55b987e3c9174ca97406e7e6e56e7bf759f
samuraimac <39005419+samuraimac@users.noreply.github.com>
2019-03-03 17:05:57 +0800

    Update AccountPanel.java

--- src/View/AccountPanel.java
+++ src/View/AccountPanel.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.File;
 import java.net.URL;
 import java.nio.file.Paths;
@@ -17,7 +19,7 @@ public class AccountPanel extends JPanel implements ActionListener {
         setOpaque(false);
         add(Box.createRigidArea(new Dimension(15,0)));
         JLabel title = new JLabel();
-        title.setFont(new Font("Arial", Font.PLAIN, 45));
+        title.setFont(new Font("Arial", Font.PLAIN, 38));
         title.setForeground(Color.white);
         add(title);
         try{
@@ -30,40 +32,70 @@ public class AccountPanel extends JPanel implements ActionListener {
 
         add(Box.createRigidArea(new Dimension(220,0)));
         addSongs = new JButton("Add Songs");
-        addSongs.setForeground(new Color(65,105,225));
+        //addSongs.setForeground(new Color(65,105,225));
+        addSongs.setForeground(Color.white);
         addSongs.setOpaque(false);
         addSongs.setContentAreaFilled(false);
         addSongs.setBorderPainted(false);
-        addSongs.setMaximumSize(new Dimension(100, 40));
-        addSongs.setMinimumSize(new Dimension(100, 40));
-        addSongs.setPreferredSize(new Dimension(100, 40));
+//        addSongs.setMaximumSize(new Dimension(120, 40));
+//        addSongs.setMinimumSize(new Dimension(120, 40));
+//        addSongs.setPreferredSize(new Dimension(120, 40));
         addSongs.setFont(new Font("Arial", Font.BOLD, 14));
         addSongs.addActionListener(this);
+        addSongs.addMouseListener(new MouseAdapter() {
+            Color oldColor = addSongs.getForeground();
+            public void mouseEntered(MouseEvent e) {
+                addSongs.setForeground(new Color(0,255,255));
+            }
+            public void mouseExited(MouseEvent e) {
+                addSongs.setForeground(oldColor);
+            }
+        });
         add(addSongs);
-        add(Box.createRigidArea(new Dimension(5,0)));
+        //add(Box.createRigidArea(new Dimension(5,0)));
 
         logIn = new JButton("Log In");
-        logIn.setForeground(new Color(65,105,225));
+        //logIn.setForeground(new Color(65,105,225));
+        logIn.setForeground(Color.white);
         logIn.setOpaque(false);
         logIn.setContentAreaFilled(false);
         logIn.setBorderPainted(false);
-        logIn.setMaximumSize(new Dimension(100, 40));
-        logIn.setMinimumSize(new Dimension(100, 40));
-        logIn.setPreferredSize(new Dimension(100, 40));
+        //logIn.setMaximumSize(new Dimension(90, 40));
+        //logIn.setMinimumSize(new Dimension(90, 40));
+        //logIn.setPreferredSize(new Dimension(90, 40));
         logIn.setFont(new Font("Arial", Font.BOLD, 14));
+        logIn.addMouseListener(new MouseAdapter() {
+            Color oldColor = logIn.getForeground();
+            public void mouseEntered(MouseEvent e) {
+                logIn.setForeground(new Color(0,255,255));
+            }
+            public void mouseExited(MouseEvent e) {
+                logIn.setForeground(oldColor);
+            }
+        });
         logIn.addActionListener(this);
         add(logIn);
-        add(Box.createRigidArea(new Dimension(5,0)));
+        //add(Box.createRigidArea(new Dimension(5,0)));
 
         signUp = new JButton("Sign Up");
-        signUp.setForeground(new Color(65,105,225));
+        //signUp.setForeground(new Color(65,105,225));
+        signUp.setForeground(Color.white);
         signUp.setOpaque(false);
         signUp.setContentAreaFilled(false);
         signUp.setBorderPainted(false);
-        signUp.setMaximumSize(new Dimension(100, 40));
-        signUp.setMinimumSize(new Dimension(100, 40));
-        signUp.setPreferredSize(new Dimension(100, 40));
+//        signUp.setMaximumSize(new Dimension(100, 40));
+//        signUp.setMinimumSize(new Dimension(100, 40));
+//        signUp.setPreferredSize(new Dimension(100, 40));
         signUp.setFont(new Font("Arial", Font.BOLD, 14));
+        signUp.addMouseListener(new MouseAdapter() {
+            Color oldColor = signUp.getForeground();
+            public void mouseEntered(MouseEvent e) {
+                signUp.setForeground(new Color(0,255,255));
+            }
+            public void mouseExited(MouseEvent e) {
+                signUp.setForeground(oldColor);
+            }
+        });
         signUp.addActionListener(this);
         add(Box.createRigidArea(new Dimension(15,0)));
         add(signUp);
@@ -72,7 +104,7 @@ public class AccountPanel extends JPanel implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if(e.getSource() == addSongs){
-            //add songs
+            AddSongWindow asw = new AddSongWindow();
         }
         if(e.getSource() == logIn){
             LogInWindow l = new LogInWindow();
