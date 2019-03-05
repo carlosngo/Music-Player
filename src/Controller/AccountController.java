7306f02186d2a6d17ef450be9e4398772ac5785c
Carlos <carlos_ngo@dlsu.edu.ph>
2019-03-05 21:06:00 +0800

    Please fill out AccountController functions :D

--- src/Controller/AccountController.java
+++ src/Controller/AccountController.java
@@ -1,12 +1,31 @@
 package Controller;
 
+import Model.*;
+import View.*;
+
+import java.util.*;
+
 public class AccountController {
+    private User user;
+    private AccountPanel accountPanel;
 
 
+    public AccountController() {
+
+    }
+
+    // logs in the user. check for errors.
     public void logIn(String username, String password) {
 
     }
 
+    // registers the user. check for errors.
+    public void register(String username, String password, String firstName, String lastName, String gender, Date birthday) {
+
+
+    }
+
+    // logs out the user. clear the cache
     public void logOut() {
 
     }
--- src/Controller/MainController.java
+++ src/Controller/MainController.java
@@ -10,23 +10,48 @@ import java.sql.*;
 
 public class MainController {
     // cache
-    private ArrayList<Genre> genres;
-    private ArrayList<Song> songs;
-    private ArrayList<Album> albums;
-    private ArrayList<Playlist> playlists;
+    private static ArrayList<Genre> genres;
+    private static ArrayList<Song> songs;
+    private static ArrayList<Album> albums;
+    private static ArrayList<Playlist> playlists;
 
     // controllers
     private AccountController ac;
     private PlayerController pc;
 
+    // daos
+    public static UserDAO userDAO;
+    public static SongDAO songDAO;
+    public static AlbumDAO albumDAO;
+    public static PlaylistDAO playlistDAO;
+    public static PlaylistSongDAO playlistSongDAO;
+    public static GenreDAO genreDAO;
+
+    // views
+    private MainScreen ms;
+
+    public MainController() {
+        DAOFactory db = new DriverManagerDAOFactory(DAOFactory.DATABASE_URL, DAOFactory.DATABASE_USERNAME, DAOFactory.DATABASE_PASSWORD);
+        userDAO = db.getUserDAO();
+        songDAO = db.getSongDAO();
+        albumDAO = db.getAlbumDAO();
+        playlistDAO = db.getPlaylistDAO();
+        playlistSongDAO = db.getPlaylistSongDAO();
+        genreDAO = db.getGenreDAO();
+    }
 
 
-
-    public void load() {
+    // saves all the cached data in the database. only used when guest logs in and chooses to save his data.
+    // therefore, assign the newly logged in user's id to all the cached data.
+    public void save() {
 
     }
 
-    public void save() {
-
+    // clears the cache
+    public static void clearCache() {
+        songs = new ArrayList<>();
+        albums = new ArrayList<>();
+        playlists = new ArrayList<>();
+        genres = new ArrayList<>();
     }
 }
