44be06fefbc0d5167df564b503fa07daf19f0c0f
John Andrew Ing <john_andrew_ing@dlsu.edu.ph>
2019-03-05 22:40:53 +0800

    AccountController initial

--- src/Controller/AccountController.java
+++ src/Controller/AccountController.java
@@ -3,7 +3,12 @@ package Controller;
 import Model.*;
 import View.*;
 
+import java.sql.SQLException;
 import java.util.*;
+
+import DAO.DAOFactory;
+import DAO.DriverManagerDAOFactory;
+import DAO.UserDAO;
 
 public class AccountController {
     private User user;
@@ -11,22 +16,40 @@ public class AccountController {
 
 
     public AccountController() {
-
+    	user = new User();
     }
 
     // logs in the user. check for errors.
     public void logIn(String username, String password) {
-
+    
+    	try {
+    		user = MainController.userDAO.find(username, password);
+    	} catch (SQLException e) {
+			e.printStackTrace();
+		}
+    
     }
 
     // registers the user. check for errors.
     public void register(String username, String password, String firstName, String lastName, String gender, Date birthday) {
-
-
+    	
+    	user.setUserName(username);
+    	user.setPassword(password);
+    	user.setFirstName(firstName);
+    	user.setLastName(lastName);
+    	user.setGender(gender);
+    	user.setBirthday(birthday);
+    	
+    	try {
+			MainController.userDAO.create(user);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
     }
 
     // logs out the user. clear the cache
     public void logOut() {
-
+    	user = null;
+    	MainController.clearCache();
     }
 }
