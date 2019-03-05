4bfb0029a52808eb5137381a70aa66ea19483b58
samuraimac <39005419+samuraimac@users.noreply.github.com>
2019-03-05 22:39:06 +0800

    Update CreateAccountWindow.java

--- src/View/CreateAccountWindow.java
+++ src/View/CreateAccountWindow.java
@@ -5,41 +5,38 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class CreateAccountWindow extends JFrame implements ActionListener, DocumentListener {
-    private JTextField usernameInput, passwordInput;
-    private JComboBox mon, day, yr;
+    private JTextField usernameInput, passwordInput, firstNameInput, lastNameInput;
+    private JComboBox mon, day, yr, gender;
     private JButton cancel, createAccount;
 
     public CreateAccountWindow(){
-        init();
-    }
-
-    public void init(){
         JPanel p = new JPanel();
         p.setLayout(new BoxLayout(p, BoxLayout.Y_AXIS));
         p.setBackground(new Color(0,0,0));
 
         p.add(Box.createRigidArea(new Dimension(0,15))); // add space
-        JLabel title = new JLabel("Create An Account");
+        JLabel title = new JLabel("Create Account");
         title.setForeground(Color.white);
         title.setAlignmentX(Component.CENTER_ALIGNMENT);
         title.setFont(new Font("Arial", Font.BOLD, 28));
         p.add(title);
         p.add(Box.createRigidArea(new Dimension(0,20))); // add space
 
+
         JPanel p1 = new JPanel();
         p1.setOpaque(false);
         p1.setLayout(new BoxLayout(p1, BoxLayout.X_AXIS));
         p1.add(Box.createRigidArea(new Dimension(15,0))); // add space
-        JLabel usernameLabel = new JLabel("Username:");
-        usernameLabel.setForeground(Color.white);
-        usernameLabel.setFont(new Font("Arial", Font.BOLD, 22));
-        p1.add(usernameLabel);
-        usernameInput = new JTextField("" , 20);
-        usernameInput.addActionListener(this);
-        usernameInput.getDocument().addDocumentListener(this);
-        usernameInput.setFont(new Font("Arial", Font.BOLD, 22));
-        //usernameInput.setBackground(new Color(152,251,152));
-        p1.add(usernameInput);
+        JLabel firstNameLabel = new JLabel("First Name:");
+        firstNameLabel.setForeground(Color.white);
+        firstNameLabel.setFont(new Font("Arial", Font.BOLD, 22));
+        p1.add(firstNameLabel);
+        firstNameInput = new JTextField("" , 10);
+        firstNameInput.addActionListener(this);
+        //firstNameInput.setEditable(false);
+        firstNameInput.setFont(new Font("Arial", Font.BOLD, 22));
+        //passwordInput.setBackground(new Color(152,251,152));
+        p1.add(firstNameInput);
         p1.add(Box.createRigidArea(new Dimension(15,0))); // add space
         p.add(p1);
         p.add(Box.createRigidArea(new Dimension(0,7))); // add space
@@ -48,16 +45,16 @@ public class CreateAccountWindow extends JFrame implements ActionListener, Docum
         p2.setOpaque(false);
         p2.setLayout(new BoxLayout(p2, BoxLayout.X_AXIS));
         p2.add(Box.createRigidArea(new Dimension(15,0))); // add space
-        JLabel passwordLabel = new JLabel("Password:");
-        passwordLabel.setForeground(Color.white);
-        passwordLabel.setFont(new Font("Arial", Font.BOLD, 22));
-        p2.add(passwordLabel);
-        passwordInput = new JTextField("" , 20);
-        passwordInput.addActionListener(this);
-        passwordInput.getDocument().addDocumentListener(this);
-        passwordInput.setFont(new Font("Arial", Font.BOLD, 22));
+        JLabel lastNameLabel = new JLabel("Last Name:");
+        lastNameLabel.setForeground(Color.white);
+        lastNameLabel.setFont(new Font("Arial", Font.BOLD, 22));
+        p2.add(lastNameLabel);
+        lastNameInput = new JTextField("" , 10);
+        lastNameInput.addActionListener(this);
+        //lastNameInput.setEditable(false);
+        lastNameInput.setFont(new Font("Arial", Font.BOLD, 22));
         //passwordInput.setBackground(new Color(152,251,152));
-        p2.add(passwordInput);
+        p2.add(lastNameInput);
         p2.add(Box.createRigidArea(new Dimension(15,0))); // add space
         p.add(p2);
         p.add(Box.createRigidArea(new Dimension(0,7))); // add space
@@ -74,7 +71,9 @@ public class CreateAccountWindow extends JFrame implements ActionListener, Docum
                 "May", "June", "July", "August", "September",
                 "October", "November", "December"};
         mon = new JComboBox(months);
+        mon.setFont(new Font("Arial", Font.PLAIN, 16));
         //mon.setBackground(new Color(152,251,152));
+        //mon.setEnabled(false);
         mon.addActionListener(this);
         p3.add(mon);
         String[] days = new String[32];
@@ -83,6 +82,8 @@ public class CreateAccountWindow extends JFrame implements ActionListener, Docum
             days[i] = Integer.toString(i);
         }
         day = new JComboBox(days);
+        day.setFont(new Font("Arial", Font.PLAIN, 16));
+        //day.setEnabled(false);
         //day.setBackground(new Color(152,251,152));
         day.addActionListener(this);
         p3.add(day);
@@ -92,6 +93,8 @@ public class CreateAccountWindow extends JFrame implements ActionListener, Docum
             years[i] = Integer.toString(1999 + i);
         }
         yr = new JComboBox(years);
+        yr.setFont(new Font("Arial", Font.PLAIN, 16));
+        //yr.setEnabled(false);
         //yr.setBackground(new Color(152,251,152));
         yr.addActionListener(this);
         p3.add(yr);
@@ -102,6 +105,60 @@ public class CreateAccountWindow extends JFrame implements ActionListener, Docum
         JPanel p4 = new JPanel();
         p4.setOpaque(false);
         p4.setLayout(new BoxLayout(p4, BoxLayout.X_AXIS));
+        p4.add(Box.createRigidArea(new Dimension(15,0))); // add space
+        JLabel genderLbl = new JLabel("Gender:");
+        genderLbl.setForeground(Color.white);
+        genderLbl.setFont(new Font("Arial", Font.BOLD, 22));
+        p4.add(genderLbl);
+        String[] genderList = {"Choose gender", "Male", "Female"};
+        gender = new JComboBox(genderList);
+        gender.setFont(new Font("Arial", Font.PLAIN, 16));
+        //gender.setEnabled(false);
+        //gender.setBackground(new Color(152,251,152));
+        gender.addActionListener(this);
+        p4.add(gender);
+        p4.add(Box.createRigidArea(new Dimension(15,0))); // add space
+        p.add(p4);
+        p.add(Box.createRigidArea(new Dimension(0,7))); // add space
+
+        JPanel p5 = new JPanel();
+        p5.setOpaque(false);
+        p5.setLayout(new BoxLayout(p5, BoxLayout.X_AXIS));
+        p5.add(Box.createRigidArea(new Dimension(15,0))); // add space
+        JLabel usernameLabel = new JLabel("Username:");
+        usernameLabel.setForeground(Color.white);
+        usernameLabel.setFont(new Font("Arial", Font.BOLD, 22));
+        p5.add(usernameLabel);
+        usernameInput = new JTextField("" , 10);
+        usernameInput.addActionListener(this);
+        //usernameInput.setEditable(false);
+        usernameInput.setFont(new Font("Arial", Font.BOLD, 22));
+        p5.add(usernameInput);
+        p5.add(Box.createRigidArea(new Dimension(15,0))); // add space
+        p.add(p5);
+        p.add(Box.createRigidArea(new Dimension(0,7))); // add space
+
+        JPanel p6 = new JPanel();
+        p6.setOpaque(false);
+        p6.setLayout(new BoxLayout(p6, BoxLayout.X_AXIS));
+        p6.add(Box.createRigidArea(new Dimension(15,0))); // add space
+        JLabel passwordLabel = new JLabel("Password:");
+        passwordLabel.setForeground(Color.white);
+        passwordLabel.setFont(new Font("Arial", Font.BOLD, 22));
+        p6.add(passwordLabel);
+        passwordInput = new JTextField("" , 10);
+        passwordInput.addActionListener(this);
+        //passwordInput.setEditable(false);
+        passwordInput.setFont(new Font("Arial", Font.BOLD, 22));
+        //passwordInput.setBackground(new Color(152,251,152));
+        p6.add(passwordInput);
+        p6.add(Box.createRigidArea(new Dimension(15,0))); // add space
+        p.add(p6);
+        p.add(Box.createRigidArea(new Dimension(0,7))); // add space
+
+        JPanel p7 = new JPanel();
+        p7.setOpaque(false);
+        p7.setLayout(new BoxLayout(p7, BoxLayout.X_AXIS));
         cancel = new JButton("Cancel");
         cancel.setFont(new Font("Arial", Font.PLAIN, 20));
         cancel.addActionListener(this);
@@ -109,18 +166,17 @@ public class CreateAccountWindow extends JFrame implements ActionListener, Docum
         cancel.setBackground(new Color(1,121,150));
         cancel.setOpaque(true);
         cancel.setBorderPainted(false);
-        p4.add(cancel);
-        p4.add(Box.createRigidArea(new Dimension(10,0))); // add space
+        p7.add(cancel);
+        p7.add(Box.createRigidArea(new Dimension(10,0))); // add space
         createAccount = new JButton("Create Account");
-        createAccount.setEnabled(false);
         createAccount.setFont(new Font("Arial", Font.PLAIN, 20));
         createAccount.addActionListener(this);
         createAccount.setForeground(Color.white);
         createAccount.setBackground(new Color(1,121,150));
         createAccount.setOpaque(true);
         createAccount.setBorderPainted(false);
-        p4.add(createAccount);
-        p.add(p4);
+        p7.add(createAccount);
+        p.add(p7);
         p.add(Box.createRigidArea(new Dimension(0,7))); // add space
 
         add(p);
@@ -133,11 +189,12 @@ public class CreateAccountWindow extends JFrame implements ActionListener, Docum
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (!usernameInput.getText().trim().equals("")) {
-            if (!passwordInput.getText().trim().equals("")) {
+        if (!usernameInput.getText().trim().equals("") && !firstNameInput.getText().trim().equals("")
+                && !lastNameInput.getText().trim().equals("") && !passwordInput.getText().trim().equals("")) {
+            if (gender.getSelectedIndex() != 0) {
                 if (yr.getSelectedIndex() != 0) {
                     if (mon.getSelectedIndex() != 0) {
-                        if (day.getSelectedIndex() != 0) {
+                        if (day.getSelectedIndex() != 0 ) {
                             createAccount.setEnabled(true);
                         } else {
                             createAccount.setEnabled(false);
@@ -154,10 +211,11 @@ public class CreateAccountWindow extends JFrame implements ActionListener, Docum
         } else {
             createAccount.setEnabled(false);
         }
-        if (e.getActionCommand().equals("Create Account")){
+
+        if (e.getSource() == createAccount){
             //create account
         }
-        if (e.getActionCommand().equals("Cancel")){
+        if (e.getSource() == cancel){
             //MainScreen caw = new MainScreen();
             dispose();
         }
@@ -166,6 +224,7 @@ public class CreateAccountWindow extends JFrame implements ActionListener, Docum
     @Override
     public void insertUpdate(DocumentEvent e) {
         if (usernameInput.getText().isEmpty() || passwordInput.getText().isEmpty() ||
+                firstNameInput.getText().isEmpty() || lastNameInput.getText().isEmpty() ||
             yr.getSelectedIndex() == 0 || mon.getSelectedIndex() == 0 || day.getSelectedIndex() == 0)
             createAccount.setEnabled(false);
         else
@@ -175,7 +234,8 @@ public class CreateAccountWindow extends JFrame implements ActionListener, Docum
     @Override
     public void removeUpdate(DocumentEvent e) {
         if (usernameInput.getText().isEmpty() || passwordInput.getText().isEmpty() ||
-            yr.getSelectedIndex() == 0 || mon.getSelectedIndex() == 0 || day.getSelectedIndex() == 0)
+                firstNameInput.getText().isEmpty() || lastNameInput.getText().isEmpty() ||
+                yr.getSelectedIndex() == 0 || mon.getSelectedIndex() == 0 || day.getSelectedIndex() == 0)
             createAccount.setEnabled(false);
         else
             createAccount.setEnabled(true);
@@ -184,7 +244,8 @@ public class CreateAccountWindow extends JFrame implements ActionListener, Docum
     @Override
     public void changedUpdate(DocumentEvent e) {
         if (usernameInput.getText().isEmpty() || passwordInput.getText().isEmpty() ||
-            yr.getSelectedIndex() == 0 || mon.getSelectedIndex() == 0 || day.getSelectedIndex() == 0)
+                firstNameInput.getText().isEmpty() || lastNameInput.getText().isEmpty() ||
+                yr.getSelectedIndex() == 0 || mon.getSelectedIndex() == 0 || day.getSelectedIndex() == 0)
             createAccount.setEnabled(false);
         else
             createAccount.setEnabled(true);
