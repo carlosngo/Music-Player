893b733151f80faf56d995a99eeebd9a08d4f3aa
Johanna Lim <johanna_lim@dlsu.edu.ph>
2019-04-03 22:23:55 +0800

    update

--- src/server/ClientThread.java
+++ src/server/ClientThread.java
@@ -1,5 +1,7 @@
 package server;
 
+import model.Server;
+
 import java.util.*;
 import java.io.*;
 import java.net.*;
@@ -26,7 +28,120 @@ public class ClientThread implements Runnable, Observer {
     }
 
     @Override
-    public void run() {
+    public void run() {System.out.println("Started thread for client.");
+        model.Server server = Server.getInstance();
+        String messageFromClient;
+        try {
+            while (!server.isShutDown() && (messageFromClient = in.readLine()) != null) {
+                System.out.println("Received the message: " + messageFromClient + " from client.");
 
+                StringBuilder reply = new StringBuilder();
+                if (messageFromClient.startsWith("GETSONGS")) {
+                    server.getSongs();
+                } else if (messageFromClient.startsWith("ADDSONG")) {
+                    server.addSong();
+                } else if (messageFromClient.equals("DELETESONG")) {
+                    server.deleteSong();
+//
+                } else if (messageFromClient.startsWith("EDITSONG")) {
+                    server.editSong();
+
+                } else if (messageFromClient.startsWith("PLAYSONG")) {
+                    server.playSong();
+
+                } else if (messageFromClient.startsWith("FOLLOWSONG")) {
+                    server.followSong();
+
+                } else if (messageFromClient.startsWith("GETPLAYLISTS")) {
+                    server.getPlaylists();
+
+                } else if (messageFromClient.startsWith("ADDPLAYLIST")) {
+                    server.addPlaylist();
+
+                } else if (messageFromClient.startsWith("DELETEPLAYLIST")) {
+                    server.deletePlaylist();
+
+                } else if (messageFromClient.startsWith("EDITPLAYLIST")) {
+                    server.editPlaylist();
+
+                } else if (messageFromClient.startsWith("PLAYPLAYLIST")) {
+                    server.playPlaylist();
+
+                } else if (messageFromClient.startsWith("FOLLOWPLAYLIST")) {
+                    server.followPlaylist();
+
+                } else if (messageFromClient.startsWith("GETALBUMS")) {
+                    server.getAlbums();
+
+                }else if (messageFromClient.startsWith("ADDALBUM")) {
+                    server.addAlbum();
+
+                }else if (messageFromClient.startsWith("DELETEALBUM")) {
+                    server.deleteAlbum();
+
+                }else if (messageFromClient.startsWith("EDITALBUM")) {
+                    server.editAlbum();
+
+                }else if (messageFromClient.startsWith("PLAYALBUM")) {
+                    server.playAlbum();
+
+                }else if (messageFromClient.startsWith("FOLLOWALBUM")) {
+                    server.followAlbum();
+
+                }else if (messageFromClient.startsWith("GETUSERS")) {
+                    server.getUsers();
+
+                }else if (messageFromClient.startsWith("ADDUSER")) {
+                    server.addUser();
+
+                }else if (messageFromClient.startsWith("EDITUSER")) {
+                    server.editUser();
+
+                }else if (messageFromClient.startsWith("FOLLOWUSER")) {
+                    server.followUser();
+
+                }else if (messageFromClient.startsWith("GETARTISTS")) {
+                    server.getArtists();
+
+                }else if (messageFromClient.startsWith("ADDARTIST")) {
+                    server.addArtist();
+
+                }else if (messageFromClient.startsWith("EDITARTIST")) {
+                    server.editArtist();
+
+                }else if (messageFromClient.startsWith("FOLLOWARTIST")) {
+                    server.followArtist();
+
+                }else if (messageFromClient.startsWith("GETIMAGEFILE")) {
+                    server.getImageFile();
+
+                }else if (messageFromClient.startsWith("SETIMAGEFILE")) {
+                    server.setImageFile();
+
+                }else if (messageFromClient.startsWith("GETSONGFILE")) {
+                    server.getSongFile();
+
+                }else if (messageFromClient.startsWith("SETSONGFILE")) {
+                    server.setSongFile();
+
+                }else if (messageFromClient.startsWith("LOGIN")) {
+                    server.login();
+
+                }else if (messageFromClient.startsWith("LOGOUT")) {
+                    server.logout();
+
+                }else if (messageFromClient.startsWith("SEARCH")) {
+                    server.getSongs();
+
+                }
+                reply.append("END");
+                System.out.println("Sending the following message to the client: " + reply.toString());
+                out.println(reply.toString());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            server.unregisterClientThread(this);
+        }
     }
 }
--- src/server/Server.java
+++ src/server/Server.java
@@ -1,6 +1,7 @@
 package server;
 
 import dao.*;
+import model.Song;
 
 import java.util.concurrent.*;
 import java.util.*;
@@ -59,6 +60,142 @@ public class Server {
     public void loadData() {
 
     }
+
+    public void addSong(Song song){
+        try {
+            //add song
+        } catch (IllegalArgumentException e) {
+            System.out.println("Song was not added.");
+        }
+    }
+
+    public void deleteSong(){
+
+    }
+
+    public void editSong(){
+
+    }
+
+    public void playSong(){
+
+    }
+
+    public void followSong(){
+
+    }
+
+    public void getPlaylists(){
+
+    }
+
+    public void addPlaylist(){
+
+    }
+
+    public void deletePlaylist(){
+
+    }
+
+    public void editPlaylist(){
+
+    }
+
+    public void playPlaylist(){
+
+    }
+
+    public void followPlaylist(){
+
+    }
+
+    public void getAlbums(){
+
+    }
+
+    public void addAlbum(){
+
+    }
+
+    public void deleteAlbum(){
+
+    }
+
+    public void editAlbum(){
+
+    }
+
+    public void playAlbum(){
+
+    }
+
+    public void followAlbum(){
+
+    }
+
+    public void getUsers(){
+
+    }
+
+    public void addUser(){
+
+    }
+
+    public void editUser(){
+
+    }
+
+    public void followUser(){
+
+    }
+
+    public void getArtists(){
+
+    }
+
+    public void addArtist(){
+
+    }
+
+    public void editArtist(){
+
+    }
+
+    public void followArtist(){
+
+    }
+
+    public void getImageFile(){
+
+    }
+
+    public void setImageFile(){
+
+    }
+
+    public void getSongFile(){
+
+    }
+
+
+    public void setSongFile(){
+
+    }
+
+
+    public void login(){
+
+    }
+
+    public void logout(){
+
+    }
+
+    public ArrayList<Song> getSongs(){
+        ArrayList<Song> dummyList = new ArrayList<Song>();
+        return dummyList;
+    }
+
 
     public static void main(String[] args) {
         Server server = Server.getInstance();
