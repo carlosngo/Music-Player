6955243a14dc767751d9610062c5220bdfb6af93
John Andrew Ing <john_andrew_ing@dlsu.edu.ph>
2019-04-08 22:46:27 +0800

    Skeleton

--- src/dao/UserAlbumDAO.java
+++ src/dao/UserAlbumDAO.java
@@ -1,5 +1,14 @@
 package dao;
 
 public class UserAlbumDAO implements DataAccessObject {
-
+	private DAOFactory db;
+	
+	private static final String SQL_INSERT = "";
+	private static final String SQL_DELETE = "";
+	private static final String SQL_LIST_BY_USER_ID = "";
+	private static final String SQL_LIST_BY_ALBUM_ID = "";
+ 	
+	public UserAlbumDAO(DAOFactory db) {
+		this.db = db;
+	}
 }
