0f2074ce0e8c069c8e7fee90e8378909e34d44f5
johanna <johanna_lim@dlsu.edu.ph>
2019-04-13 15:13:51 +0800

    Update UserPanel.java

--- src/view/UserPanel.java
+++ src/view/UserPanel.java
@@ -108,7 +108,7 @@ public class UserPanel extends CategoryPanel {
         play.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                controller.
+                controller.p
             }
         });
 
